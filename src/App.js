065df63806e4331225b28fa8a56495ed5d389f15
AlbertoCazallasMonje <alberto.cazallas@alu.uclm.es>
2025-03-08 13:14:09 +0100

    Login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/home/home';
 import './App.css';
+import Login from "./components/login/login";
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Routes>
-          {/* Route for the home page */}
+          {/* Route for the home page + Login + Register */}
           <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+
+            {/* Redirect to '/' if page does not exist */}
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
-
-
     </div>
   );
 }
