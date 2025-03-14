95f13f2f02e563541563db8773aeee0731f4efc7
AlbertoCazallasMonje <alberto.cazallas@alu.uclm.es>
2025-03-14 15:08:04 +0100

    Update page

--- src/components/update/updateUser.js
+++ src/components/update/updateUser.js
@@ -1,63 +1,52 @@
-import React, {useState, useEffect} from "react";
-import './register.css';
-import {countries} from '../utils/countries';
-import {useNavigate} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import './updateUser.css';
+import { countries } from '../utils/countries';
+import { useNavigate } from 'react-router-dom';
 
-const Register = () => {
+const UpdateProfile = () => {
     const navigate = useNavigate();
+
+    const [dni, setDni] = useState("");
     const [name, setName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [dni, setDni] = useState("");
     const [age, setAge] = useState("");
     const [address, setAddress] = useState("");
     const [country, setCountry] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                const response = await fetch("/findUser", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
 
-        if (!isValidEmail(email)) {
-            alert("Please, introduce a valid Email.");
-            return;
-        }
-        if (!isValidPassword(password)) {
-            alert("The password must be 8 characters long, include one uppercase and one special character.");
-            return;
-        }
-
-        const payload = {
-            dni,
-            name: name,
-            lastName: lastName,
-            age: parseInt(age, 10),
-            email,
-            password,
-            address,
-            country
+                if (response.ok) {
+                    const data = await response.json();
+                    setDni(data.dni);
+                    setName(data.name);
+                    setLastName(data.lastName);
+                    setAge(data.age);
+                    setAddress(data.address);
+                    setCountry(data.country);
+                    setEmail(data.email);
+                } else {
+                    alert("Error while retrieving user data.");
+                }
+            } catch (error) {
+                console.error("Error while retrieving user data:", error);
+                alert("Error while retrieving user data");
+            }
         };
 
-        try {
-            const response = await fetch("/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(payload)
-            });
-
-            if (response.ok) {
-                alert("User registered successfully.");
-                navigate('/login')
-            } else {
-                const errorData = await response.json();
-                alert("Error while registering a new user: " + errorData.message);
-            }
-        } catch (error) {
-            console.error("Error while registering a new user:", error);
-            alert("Error while registering a new user.");
-        }
-    };
+        fetchUserData();
+    }, []);
 
     const isValidEmail = (email) => {
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -68,19 +57,68 @@ const Register = () => {
         const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
         return regex.test(password);
     };
-    const handleBack = () => {
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!isValidEmail(email)) {
+            alert("Please, introduce a valid email format.");
+            return;
+        }
+
+        if (password && !isValidPassword(password)) {
+            alert("The password must be 8 characters long, include one uppercase and one special character.");
+            return;
+        }
+
+        const payload = {
+            dni,
+            name,
+            lastName,
+            age: parseInt(age, 10),
+            email,
+            address,
+            country,
+            ...(password && { password })
+        };
+
+        try {
+            const token = localStorage.getItem("token");
+            const response = await fetch("/update", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+                body: JSON.stringify(payload)
+            });
+
+            if (response.ok) {
+                alert("Data updated successfully.");
+                navigate("/profile");
+            } else {
+                const errorData = await response.json();
+                alert("Error while updating user data: " + errorData.message);
+            }
+        } catch (error) {
+            console.error("Error while updating user data", error);
+            alert("Error while updating user data.");
+        }
+    };
+
+    const handleCancel = () => {
         navigate(-1);
     };
 
     return (
-        <div className="registro-page">
+        <div className="update-page">
             <header className="header">
                 <nav className="navbar">
                     <div className="navbar-logo">
                         <h2>Orion</h2>
                     </div>
                     <div className="navbar-buttons">
-                        <button className="btn back" onClick={handleBack}>
+                        <button className="btn back" onClick={handleCancel}>
                             Go Back
                         </button>
                     </div>
@@ -88,15 +126,26 @@ const Register = () => {
             </header>
 
             <main className="main">
-                <div className="registro-container">
-                    <h2>Register</h2>
+                <div className="update-container">
+                    <h2>Update User Profile</h2>
                     <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="dni">DNI</label>
+                            <input
+                                type="text"
+                                id="dni"
+                                placeholder="DNI"
+                                required
+                                value={dni}
+                                disabled
+                            />
+                        </div>
                         <div className="form-group">
                             <label htmlFor="name">Name</label>
                             <input
                                 type="text"
                                 id="name"
-                                placeholder="Type your First Name"
+                                placeholder="Type your name"
                                 required
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
@@ -107,21 +156,10 @@ const Register = () => {
                             <input
                                 type="text"
                                 id="lastName"
-                                placeholder="Type your Last Name"
+                                placeholder="Type your last name"
                                 required
                                 value={lastName}
                                 onChange={(e) => setLastName(e.target.value)}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="dni">DNI</label>
-                            <input
-                                type="text"
-                                id="dni"
-                                placeholder="Type your DNI"
-                                required
-                                value={dni}
-                                onChange={(e) => setDni(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
@@ -129,7 +167,7 @@ const Register = () => {
                             <input
                                 type="number"
                                 id="age"
-                                placeholder="Type your Age"
+                                placeholder="Type your age"
                                 required
                                 value={age}
                                 onChange={(e) => setAge(e.target.value)}
@@ -140,7 +178,7 @@ const Register = () => {
                             <input
                                 type="text"
                                 id="address"
-                                placeholder="Type your Address"
+                                placeholder="Type your address"
                                 required
                                 value={address}
                                 onChange={(e) => setAddress(e.target.value)}
@@ -154,7 +192,7 @@ const Register = () => {
                                 value={country}
                                 onChange={(e) => setCountry(e.target.value)}
                             >
-                                <option value="">Select a country</option>
+                                <option value="">Select your country</option>
                                 {countries.map((c) => (
                                     <option key={c.code} value={c.code}>
                                         {c.name}
@@ -163,39 +201,39 @@ const Register = () => {
                             </select>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="email">Email</label>
+                            <label htmlFor="email">CEmail</label>
                             <input
                                 type="email"
                                 id="email"
-                                placeholder="Type your Email"
+                                placeholder="Type your email"
                                 required
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="password">Password</label>
+                            <label htmlFor="password">Password (Don't fill to keep the old credentials)</label>
                             <input
                                 type="password"
                                 id="password"
-                                placeholder="Type your password"
-                                required
+                                placeholder="Type a new password if you want to change it"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             />
                         </div>
-                        <button type="submit" className="btn register-btn">
-                            Sign-up
-                        </button>
+                        <div className="button-group">
+                            <button type="submit" className="btn update-btn">
+                                Accept
+                            </button>
+                            <button type="button" className="btn cancel-btn" onClick={handleCancel}>
+                                Cancel
+                            </button>
+                        </div>
                     </form>
                 </div>
             </main>
-
-            <footer className="footer">
-                <p>&copy; 2025 Orion</p>
-                <p>Alberto Cazallas Monje</p>
-            </footer>
         </div>
     );
-}
-export default Register;
+};
+
+export default UpdateProfile;
