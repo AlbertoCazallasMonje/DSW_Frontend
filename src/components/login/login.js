b8f3e0c325fd17319fdb67271f325ae817336b9f
AlbertoCazallasMonje <alberto.cazallas@alu.uclm.es>
2025-03-22 15:15:29 +0100

    Dashboard first review

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -28,7 +28,7 @@ const Login = () => {
             if (response.ok) {
                 const data = await response.json();
                 console.log('Login successful:', data);
-                navigate('/update', { state: { token: data.token } });
+                navigate('/dashboard', { state: { token: data.token } });
             } else {
                 console.error('Login failed:', response.statusText);
             }
